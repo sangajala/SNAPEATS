3955dee35b529cd5802898c93453b8b7de33c510
sangajala <sriram.angajala@gmail.com>
2018-03-13 15:23:16 +0000

    Updated the tag to run all tests

--- src/test/java/step_definitions/RunCukesTest.java
+++ src/test/java/step_definitions/RunCukesTest.java
@@ -1,5 +1,6 @@
 package step_definitions;
 
+import modules.HotelsData;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
@@ -10,9 +11,11 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features = "classpath:features",
 		plugin = {"pretty", "html:target/cucumber-html-report"},
-		tags = {"@admin5"}
+		tags = {"@hotel"}
 		)
 public class RunCukesTest{
 	public static String browser = "chrome";
-	public static boolean remote = false;
+	public static boolean remote = true;
+	public static String url = "https://hotels-staging.eurostar.com";
+	public static HotelsData hotelsData = new HotelsData();
 }
